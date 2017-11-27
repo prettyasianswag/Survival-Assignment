b75bf82a8352c9db02105a25e4c21a7ff5491e52
Alexander Zhang <rich_zhanga@outlook.com>
2017-11-27 11:49:54 +1100

    Testing Player_Jump

---
+++ Assets/!Test/Scripts/Test.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Test : MonoBehaviour
+{
+    public float checkDelay = 1f;
+
+    private float checkTimer = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Simulate test first
+        Simulate();
+
+        // Check for the check function
+        checkTimer += Time.deltaTime;
+        if (checkTimer >= checkDelay)
+        {
+            Check();
+            checkTimer = 0;
+        }
+
+        // Perform debugging
+        Debug();
+    }
+
+    public virtual void Debug() { } // For GizmosGL to perform debugging
+    public virtual void Simulate() { } // Run once per frame
+    public abstract void Check(); // Perform checks to see whether a test has succeeded or failed
+}
---
+++ Assets/!Test/Scripts/Test_Jump.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using GGL; // << ADDED THIS
+
+[RequireComponent(typeof(PlayerController))]
+public class Test_Jump : Test
+{
+    [Header("Test Parameters")]
+    public float minHeight = 1f;
+
+    private PlayerController player;
+    private float originalY;
+    private float jumpApex;
+
+    void Start()
+    {
+        player = GetComponent<PlayerController>();
+        originalY = transform.position.y;
+    }
+
+    public override void Simulate()
+    {
+        // Simulate jump mechanic on the player
+        player.Jump();
+    }
+
+    public override void Check()
+    {
+        // Get current player Y position
+        float playerY = player.transform.position.y;
+        float height = playerY - originalY;
+        // Get the heighest point that player jumps to (the apex)
+        if (height > jumpApex)
+            jumpApex = height;
+
+        // Check if the Y went up from original position
+        if (jumpApex > minHeight)
+        {
+            // The test has succeeded
+            IntegrationTest.Pass(gameObject);
+        }
+    }
+
+    public override void Debug()
+    {
+        GizmosGL.color = Color.red;
+        Vector3 originalPos = new Vector3(0, originalY, 0);
+        Vector3 playerPos = transform.position;
+
+        // Draw the min height the player needs to jump to
+        GizmosGL.AddLine(originalPos, originalPos + Vector3.up * minHeight);
+
+        // Draw the player's current height
+        GizmosGL.AddLine(originalPos, playerPos, 0.35f, 0.35f);
+    }
+}
